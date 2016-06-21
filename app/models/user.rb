17df016b8a65dbdd971984f81ae81318392c8d5e
gautamsawhney <gsawhney21@gmail.com>
2016-06-21 08:00:50 -0400

    Added Roles to User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+  enum roles: [:restaurant_owner, :super_admin, :mod]
 
 # Include default devise modules. Others available are:
 # :confirmable, :lockable, :timeoutable and :omniauthable
