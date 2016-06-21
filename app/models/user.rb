17eb2e000819ce8b021feadb5391d23ba408265d
gautamsawhney <gsawhney21@gmail.com>
2016-06-21 07:56:50 -0400

    Added Relationships for models

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,7 @@ class User < ActiveRecord::Base
         :recoverable, :rememberable, :trackable 
 
   # Autocode: Relationships
+  has_many :posts
   has_many :restaurants
   
   has_many :authtokens, dependent: :destroy
