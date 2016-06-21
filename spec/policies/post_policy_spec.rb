56ecae9ad1a00ac16a1828ebe94c92f59e90da2e
gautamsawhney <gsawhney21@gmail.com>
2016-06-21 08:01:01 -0400

    Added Pundit Policy Specs

--- spec/policies/post_policy_spec.rb
+++ spec/policies/post_policy_spec.rb
@@ -6,5 +6,31 @@ describe PostPolicy do
   let(:post) { Post.create }
 
   #add_here
+context "being a/an mod" do
+                          let(:user) {  User.create('roles' => 'mod', 'email' => 'first@first.com', 'password' => 'abcdabcd') }
+                          # Action Specs :: Mod
+it { should permit_action(:destroy) }
+it { should permit_action(:update) }
+it { should permit_action(:show) }
+it { should permit_action(:create) }
+                          # Attribute Specs :: Mod
+                        end
+context "being a/an super_admin" do
+                          let(:user) {  User.create('roles' => 'super_admin', 'email' => 'first@first.com', 'password' => 'abcdabcd') }
+                          # Action Specs :: SuperAdmin
+it { should permit_action(:destroy) }
+it { should permit_action(:update) }
+it { should permit_action(:show) }
+it { should permit_action(:create) }
+                          # Attribute Specs :: SuperAdmin
+                        end
+context "being a/an restaurant_owner" do
+                          let(:user) {  User.create('roles' => 'restaurant_owner', 'email' => 'first@first.com', 'password' => 'abcdabcd') }
+                          # Action Specs :: RestaurantOwner
+it { should permit_action(:update) }
+it { should permit_action(:show) }
+it { should permit_action(:create) }
+                          # Attribute Specs :: RestaurantOwner
+                        end
 
 end
--- spec/policies/restaurant_policy_spec.rb
+++ spec/policies/restaurant_policy_spec.rb
@@ -6,5 +6,22 @@ describe RestaurantPolicy do
   let(:restaurant) { Restaurant.create }
 
   #add_here
+context "being a/an super_admin" do
+                          let(:user) {  User.create('roles' => 'super_admin', 'email' => 'first@first.com', 'password' => 'abcdabcd') }
+                          # Action Specs :: SuperAdmin
+it { should permit_action(:destroy) }
+it { should permit_action(:update) }
+it { should permit_action(:show) }
+it { should permit_action(:create) }
+                          # Attribute Specs :: SuperAdmin
+                        end
+context "being a/an restaurant_owner" do
+                          let(:user) {  User.create('roles' => 'restaurant_owner', 'email' => 'first@first.com', 'password' => 'abcdabcd') }
+                          # Action Specs :: RestaurantOwner
+it { should permit_action(:update) }
+it { should permit_action(:show) }
+it { should permit_action(:create) }
+                          # Attribute Specs :: RestaurantOwner
+                        end
 
 end
