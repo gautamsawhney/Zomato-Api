ed00f09692c1a4cd28a50f2a2926faa3e8251870
gautamsawhney <gsawhney21@gmail.com>
2016-06-21 07:56:38 -0400

    Added the required Devise code to all the files -- Username Support

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,9 +1,10 @@
 FactoryGirl.define do
 
-	sequence(:email) { |n| "user#{n}@example.com" }
-	
-	factory :user do
+  sequence(:email) { |n| "user#{n}@example.com" }
+  sequence(:username) { |n| "user#{n}" }
+  factory :user do
     email
+    username
     password '12345678'
     password_confirmation '12345678'
     #roles :admin
@@ -12,21 +13,23 @@ FactoryGirl.define do
   #roles
 factory :mod, class: User do
                     email
+                    username
                     password '12345678'
                     password_confirmation '12345678'
                     roles :mod
                   end
 factory :super_admin, class: User do
                     email
+                    username
                     password '12345678'
                     password_confirmation '12345678'
                     roles :super_admin
                   end
 factory :restaurant_owner, class: User do
                     email
+                    username
                     password '12345678'
                     password_confirmation '12345678'
                     roles :restaurant_owner
                   end
-  
 end
